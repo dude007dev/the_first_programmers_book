e50aa06e546606fd175783c64976c4ccf252fceb
Dude007Dev <dude007dev@gmail.com>
2025-09-12 22:34:23 +0200

    updates;

---
+++ chapters/06/ua/6_code_samples.py
@@ -0,0 +1,16 @@
+# === 6.2 Inline comment ===
+input_value = input("What is your name?")
+name = input_value.title()  # Робимо перші літери великими
+print(f"Hello {name}!")
+
+# === 6.3 Block comment, variant 1 ===
+input_value = input("What is your name?")
+name = input_value.title()
+# print(f"Hello {name}!")
+
+# === 6.3 Block comment, variant 2 ===
+input_value = input("What is your name?")
+# Робимо перші літери великими
+# name = input_value.title()
+# Виводимо привітання
+# print(f"Hello {name}!")
