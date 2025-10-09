520861c40470464530aacf9955233d0ead1f401a
Andrii Voronov <voronov.a.g@gmail.com>
2025-10-09 17:01:55 +0200

    updated chapter 19;

--- chapters/19/ua/19_guess_the_number_game.py
+++ chapters/19/ua/19_guess_the_number_game.py
@@ -1,7 +1,5 @@
 # 19. Гра “Вгадай число”
 
-# 19.2 Базовий варіант гри
-
 import random
 
 print("Welcome to the Number Guessing Game!")
@@ -11,7 +9,12 @@ print("You have 5 attempts to guess it.")
 secret_number = random.randint(1, 100)
 
 for attempt in "12345":  # Надаємо гравцеві 5 спроб
-    guess = int(input("Enter your guess: "))
+    guess = None
+    while guess is None:
+        try:
+            guess = int(input("Enter your guess: "))
+        except ValueError:
+            print("Invalid input. Please enter a whole number.")
 
     if guess < secret_number:
         print("Too low. Try a higher number.")
