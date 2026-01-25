5f898ca19046b8f39ac59f03ff2a86928502ff65
dude007dev <dude007dev@gmail.com>
2026-01-25 10:40:25 +0100

    added chapter 11;

---
+++ chapters/11/en/11_deposit_profit_program.py
@@ -0,0 +1,14 @@
+# 11.5 Program for calculating deposit profit
+
+d = 1500
+i = 1.35
+n = 3
+
+interest_rate = i / 100
+compound_interest = d * ((1 + interest_rate) ** n - 1)
+
+result = d + compound_interest
+print(result)  # 1561.5738155625004
+
+result = round(result, 2)
+print(result)  # 1561.57
