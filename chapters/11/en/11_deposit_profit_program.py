34a784f43716aedfb668d5c7807fae56c48132aa
dude007dev <dude007dev@gmail.com>
2026-08-01 11:48:28 +0200

    minor fixes and improvements;

--- chapters/11/en/11_deposit_profit_program.py
+++ chapters/11/en/11_deposit_profit_program.py
@@ -7,8 +7,8 @@ n = 3
 interest_rate = i / 100
 compound_interest = d * ((1 + interest_rate) ** n - 1)
 
-result = d + compound_interest
-print(result)  # 1561.5738155625004
+total_amount = d + compound_interest
+print(total_amount)  # 1561.5738155625004
 
-result = round(result, 2)
-print(result)  # 1561.57
+total_amount = round(total_amount, 2)
+print(total_amount)  # 1561.57
